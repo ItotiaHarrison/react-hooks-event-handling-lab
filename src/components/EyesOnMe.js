014bd714ee2b2865e3945275af299f633ab5ffdd
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-09-22 23:56:20 +0300

    incomplete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function handleClick(event) {
+        if (event===focus) {
+            console.log('Good!')
+        }
+        if (event===blur) {
+            console.log('Hey! Eyes on me!')
+        }
+    }
+        return (
+            <div>
+                <button onClick={handleClick(blur)}>Eyes on me</button>
+            </div>
+        );
+
+    
+}
+
+
+export default EyesOnMe;
